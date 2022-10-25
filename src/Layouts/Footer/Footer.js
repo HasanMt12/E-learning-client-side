8e5df677e10cc7059dd458b4a87cf5c7c818cdce
HasanMt12 <hasan.2mahmud22gmail.com>
2022-10-25 18:17:28 +0600

    navber and footer added

--- src/Layouts/Footer/Footer.js
+++ src/Layouts/Footer/Footer.js
@@ -3,8 +3,19 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            <h1>this footer</h1>
+            <div className="bg-dark text-light text-center py-5 ">
+        <div className="container">
+            <img src="images/logo1.png" alt=""/>
+            <p>Office 78, Dhaka, Azimpur, Bangladesh</p>
+            <p>Privacy Ploicy | Terms of use</p>
         </div>
+        <div>
+            
+        </div>
+
+        </div>
+    </div>
+        
     );
 };
 
