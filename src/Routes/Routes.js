894100bf34f56b02c9e2cad25c918400c2fafcfb
HasanMt12 <hasan.2mahmud22gmail.com>
2022-10-25 18:06:04 +0600

    routes set up and layouts set and home components add

---
+++ src/Routes/Routes.js
@@ -0,0 +1,15 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from '../Layouts/Main'
+import Home from "../Pages/Home/Home";
+export const routes = createBrowserRouter([
+    {
+        path:'/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            }
+        ]
+    }
+])
