a2cbf27575a65862732775d7466971cc3eb1177b
HasanMt12 <hasan.2mahmud22gmail.com>
2022-10-25 17:28:44 +0600

    react app create and firebase.confif file added basic firebase set up

---
+++ src/firebaseConfig/firebase.config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import {
+    initializeApp
+} from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyB67uV6ofU2b6CuJx5SOm1pW9MPX58JZvE",
+    authDomain: "e-learning-client-side-b-6.firebaseapp.com",
+    projectId: "e-learning-client-side-b-6",
+    storageBucket: "e-learning-client-side-b-6.appspot.com",
+    messagingSenderId: "287459363745",
+    appId: "1:287459363745:web:532b4f30e7b819bb7dd9e0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
+
